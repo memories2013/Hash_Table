c020ff1be33422aef214b2e7f665b3e997161ff9
memories2013 <nshivaprasad41@gmail.com>
2021-12-03 17:19:42 +0530

    Refactor code to avoid the word Avoidable

--- src/com/hash/HashTable.java
+++ src/com/hash/HashTable.java
@@ -62,10 +62,11 @@ public class HashTable {
 		this.size = str.split(" ").length;
 		table = new ListNode[size];
 		for( String word : str.split(" ")) {
+			if(word.equals("avoidable"))
+				continue;
 			System.out.println(" word: " + word);
 			put(word.toLowerCase());
 		}
 		display();
 	}
 }
-	
